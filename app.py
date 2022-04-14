407d487f89734888342a3fe203f520cb5db5cecf
sungjie <74813460+sungjie@users.noreply.github.com>
2022-04-14 13:49:10 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+import datetime
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Survey route
+@app.route("/")
+def survey():
+    return render_template("survey.html")
+
+@app.route("/anthro")
+def anthro():
+    return None
